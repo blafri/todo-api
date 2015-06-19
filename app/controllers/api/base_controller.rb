257116247c21e93fc61e7fdecf44278102b91e70
Blayne Farinha <blayne.farinha@gmail.com>
2015-06-19 00:50:41 +0000

    User Authentication Complete

---
+++ app/controllers/api/base_controller.rb
@@ -0,0 +1,19 @@
+module Api
+  # Public: Base api controller that all other api controllers inherit from
+  class BaseController < ApplicationController
+    skip_before_action :verify_authenticity_token
+
+    private
+
+    # Internal: Checks if the credentials the user provided for login are valid
+    #
+    # Returns the user object for the credentials supplied or false if the
+    #         credentials are not valid
+    def authenticated?
+      authenticate_with_http_basic do |username, password|
+        user = User.find_by_user_name(username)
+        user && user.authenticate(password)
+      end
+    end
+  end
+end
